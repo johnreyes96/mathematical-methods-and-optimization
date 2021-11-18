f16d45ecfde48678335741a8274d997e3672e1bd
johnfreyes <jhonfer96@hotmail.com>
2021-11-18 07:42:05 -0500

    update formula in quadractic interpolation

--- src/main/python/quadraticinterpolation/main.py
+++ src/main/python/quadraticinterpolation/main.py
@@ -1,20 +1,20 @@
-def formula(x, fx):
-    b0 = fx[0]
-    b1 = (fx[1] - fx[0]) / (x[1] - x[0])
-    b2 = (((fx[2] - fx[1]) / (x[2] - x[1])) - ((fx[1] - fx[0]) / (x[1] - x[0]))) / (x[2] - x[0])
-    f2 = b0 + b1 * (x[3] - x[0]) + b2 * (x[3] - x[0]) * (x[3] - x[1])
-    return f2
+import math
 
 
-x = []
-fx = []
+def formula(x):
+    numerator = f(x[0]) * (x[1] ** 2 - x[2] ** 2) + f(x[1]) * (x[2] ** 2 - x[0] ** 2) + f(x[2]) * (x[0] ** 2 - x[1] ** 2)
+    denominator = 2 * f(x[0]) * (x[1] - x[2]) + 2 * f(x[1]) * (x[2] - x[0]) + 2 * f(x[2]) * (x[0] - x[1])
+    return numerator / denominator
 
-for i in range(0, 3):
-    vx = float(input("Introduce el valor para x[" + str(i + 1) + "] "))
-    x.append(vx)
-    vfx = float(input("Introduce el valor para fx[" + str(i + 1) + "] "))
-    fx.append(vfx)
 
-vx = float(input("Introduce el valor de x para calcular f(x): "))
-x.append(vx)
-print("El valor de f(x) es: ", formula(x, fx))
+def f(value):
+    return 2 * math.sin(value) - (value ** 2 / 10)
+
+
+xValues = []
+
+for index in range(0, 3):
+    xValue = float(input("Introduce el valor para x[" + str(index) + "] "))
+    xValues.append(xValue)
+
+print("El valor de f(x) es: ", formula(xValues))
