3d7f1ff54f441af366489a1f70ce1a6036d915e9
johnfreyes <jhonfer96@hotmail.com>
2021-11-18 07:00:43 -0500

    fixed code smell

--- src/main/python/quadraticinterpolation/main.py
+++ src/main/python/quadraticinterpolation/main.py
@@ -3,7 +3,8 @@ def formula(x, fx):
     b1 = (fx[1] - fx[0]) / (x[1] - x[0])
     b2 = (((fx[2] - fx[1]) / (x[2] - x[1])) - ((fx[1] - fx[0]) / (x[1] - x[0]))) / (x[2] - x[0])
     f2 = b0 + b1 * (x[3] - x[0]) + b2 * (x[3] - x[0]) * (x[3] - x[1])
-    return (f2)
+    return f2
+
 
 x = []
 fx = []
@@ -13,6 +14,7 @@ for i in range(0, 3):
     x.append(vx)
     vfx = float(input("Introduce el valor para fx[" + str(i + 1) + "] "))
     fx.append(vfx)
+
 vx = float(input("Introduce el valor de x para calcular f(x): "))
 x.append(vx)
 print("El valor de f(x) es: ", formula(x, fx))
