38fca4d2e7a417f56ca5d30116db66e78441204c
johnfreyes <jhonfer96@hotmail.com>
2022-04-22 23:44:45 -0500

    change upper triangule of matrix to mirror matrix across diagonal, code smell, sort ndarray and add weight from hamiltonian cycles

--- src/main/python/operationsresearch/npcompleteness/NPCompleteness.py
+++ src/main/python/operationsresearch/npcompleteness/NPCompleteness.py
@@ -2,14 +2,16 @@ from builtins import len
 import numpy as np
 
 
-def upperTriangularMatrixWithMainDiagonalAtZero(matrix):
-    return np.triu(matrix, 1)
+def upperTriangularMatrixWithMainDiagonalAtZero(matrixNxN):
+    UpperTriangleMatrix = np.triu(matrixNxN, 1)
+    return UpperTriangleMatrix + UpperTriangleMatrix.T - np.diag(np.diag(UpperTriangleMatrix))
 
 
-def bruteForceSearch(nodesCount, hamiltonianCyclesList=[], hamiltonianCycle="", initialIndex=-1, flag=True):
+def bruteForceSearch(nodesCount, hamiltonianCyclesList=None, hamiltonianCycle="", initialIndex=-1, flag=True):
     global nodesTotal
     if flag:
         nodesTotal = nodesCount
+        hamiltonianCyclesList = []
         flag = False
     for index in range(nodesCount):
         if index == 0:
@@ -19,8 +21,11 @@ def bruteForceSearch(nodesCount, hamiltonianCyclesList=[], hamiltonianCycle="", 
         if nodesCount == 1 and index == 0:
             hamiltonianCycle += str("-") + str(initialIndex)
             formattedHamiltonianCycle = getHamiltonianCycle(hamiltonianCycle)
-            hamiltonianCyclesList.append(formattedHamiltonianCycle)
-            print(str(len(hamiltonianCyclesList)) + ": " + str(formattedHamiltonianCycle))
+            hamiltonianCycleWeight = getWeightFromHamiltonianCycle(formattedHamiltonianCycle)
+            hamiltonianCyclesList.append({
+                'cycle': formattedHamiltonianCycle,
+                'weight': hamiltonianCycleWeight
+            })
             hamiltonianCycle = ""
         if nodesCount == nodesTotal:
             initialIndex = index
@@ -48,9 +53,20 @@ def getHamiltonianCycle(cycle):
     return hamiltonianCycle
 
 
+def getWeightFromHamiltonianCycle(formattedHamiltonianCycle):
+    return 0
+
+
+def sortHamiltonianCyclesByWeight(hamiltonianCyclesList):
+    return hamiltonianCyclesList['weight']
+
+
 # a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 matrix = np.array([[0, 5, 9, 7], [0, 0, 4, 10], [0, 0, 0, 12], [0, 0, 0, 0]])
 adjacencyMatrix = upperTriangularMatrixWithMainDiagonalAtZero(matrix)
-print(matrix)
-nodesTotal = len(matrix)
-print("cantidad de ciclos Hamiltonianos: " + str(len(bruteForceSearch(nodesTotal))))
+print(adjacencyMatrix)
+nodesTotal = len(adjacencyMatrix)
+hamiltonianCycles = bruteForceSearch(nodesTotal)
+hamiltonianCycles.sort(key=sortHamiltonianCyclesByWeight)
+print(*hamiltonianCycles, sep="\n")
+print("Cantidad de ciclos Hamiltonianos: " + str(len(hamiltonianCycles)))
