7098a5cc79d70e5ec0b976c95b39709de176bee7
johnfreyes <jhonfer96@hotmail.com>
2022-04-27 23:57:42 -0500

    min value of ndarray for each node

--- src/main/python/operationsresearch/nearestneighbor/NearestNeighbor.py
+++ src/main/python/operationsresearch/nearestneighbor/NearestNeighbor.py
@@ -6,17 +6,37 @@ def upperTriangularMatrixWithMainDiagonalAtZero(matrixNxN):
     return UpperTriangleMatrix + UpperTriangleMatrix.T - np.diag(np.diag(UpperTriangleMatrix))
 
 
-def NearestNeighbor(nodesCount):
-    for index in range(nodesCount):
-        print(index)
+def nearestNeighbor(adjacencyMatrix):
+    for indexRow, row in enumerate(adjacencyMatrix):
+        rowToCompare = row
+        for node in range(len(row)):
+            print(str(rowToCompare)+": "+str(np.min(rowToCompare)))
+            if node == 0:
+                hamiltonianCycle = str(np.where(rowToCompare == 0)[0][0])
+                # hamiltonianCycle = str("-") + str(column)
+                rowToCompare = np.delete(rowToCompare, np.where(rowToCompare == np.min(rowToCompare))[0][0])
+            else:
+                rowToCompare = np.delete(rowToCompare, np.where(rowToCompare == np.min(rowToCompare))[0][0])
+            # rowToCompare = np.delete(rowToCompare, np.min(rowToCompare))
+            # print(rowToCompare)
+            # print(str(rowToCompare)+": "+str(np.min(rowToCompare)))
+            # print(str(np.min(row))+"|"+str(np.where(row == 0)[0][0]))
+        print("\n")
 
 
 def runNearestNeighborAlgorithm():
+    matrix7x7 = np.array(
+        [[0, 10, 8, 5, 7, 20, 15],
+         [0, 0, 12, 16, 9, 13, 4],
+         [0, 0, 0, 18, 22, 30, 25],
+         [0, 0, 0, 0, 13, 14, 24],
+         [0, 0, 0, 0, 0, 32, 36],
+         [0, 0, 0, 0, 0, 0, 50],
+         [0, 0, 0, 0, 0, 0, 0]]
+    )
+    adjacencyMatrix = upperTriangularMatrixWithMainDiagonalAtZero(matrix7x7)
     print(str(adjacencyMatrix)+"\n")
-    NearestNeighbor(nodesTotal)
+    nearestNeighbor(adjacencyMatrix)
 
 
-matrix7x7 = np.array([[0, 10, 8, 5, 7, 20, 15], [0, 0, 12, 16, 9, 13, 4], [0, 0, 0, 18, 22, 30, 25], [0, 0, 0, 0, 13, 14, 24], [0, 0, 0, 0, 0, 32, 36], [0, 0, 0, 0, 0, 0, 50], [0, 0, 0, 0, 0, 0, 0]])
-adjacencyMatrix = upperTriangularMatrixWithMainDiagonalAtZero(matrix7x7)
-nodesTotal = len(adjacencyMatrix)
 runNearestNeighborAlgorithm()
